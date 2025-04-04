e4419d1268702ca3c29f3f9d303a3891cecbf55f
Aparna-015 <Aparnam@1995>
2025-04-04 23:37:29 +0530

    added azeem mail id

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,4 +1,4 @@
 module.exports ={
-    emails:["harikumarv9000@gmail.com","harikumarv7000@gmail.com","mssvsahal@gmail.com","azeemfayiz952@gmail.com"],
+    emails:["harikumarv9000@gmail.com","harikumarv7000@gmail.com","mssvsahal@gmail.com","azeemfayiz952@gmail.com","isledays786@gmail.com"],
     phones:["8547955809","9344963795","80785496682","8330018044"]
 }

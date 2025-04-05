6751da78eb8c821ed1200f9913994c1b33ced6f5
Aparna-015 <Aparnam@1995>
2025-04-05 15:41:37 +0530

    cors test hari

--- src/app.js
+++ src/app.js
@@ -18,18 +18,19 @@ const allowedOrigins = [
   "http://localhost:3000",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.log("⛔ Blocked origin by CORS:", origin);
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       console.log("⛔ Blocked origin by CORS:", origin);
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// }));
 
+app.use(cors())
 // ✅ Middleware
 app.use(cookieParser());
 app.use(express.json({ limit: "2gb" }));
